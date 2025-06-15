cb69cea821c6fb673dcfdb60726eee507538fb55
saeed <git@saeedc.com>
2025-06-15 08:31:13 -0400

    Fix: Reorganize nushell configuration into nu files Feat: Add custom command to parse `opam env` for nu

---
+++ modules/core/nushell/default.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  environment.systemPackages = with pkgs; [
+    fish
+    zoxide
+  ];
+
+  home-manager.sharedModules = [
+    {
+      xdg.configFile = {
+        "nushell/zoxide.nu".source = pkgs.runCommand "zoxide.nu" { } ''
+          ${lib.getExe pkgs.zoxide} init nushell --cmd cd > $out
+        '';
+
+        "nushell/ls_colors.txt".source = pkgs.runCommand "ls_colors.txt" { } ''
+          ${lib.getExe pkgs.vivid} generate modus-operandi > $out
+        '';
+
+        "nushell/starship.nu".source = pkgs.runCommand "starship.nu" { } ''
+          ${lib.getExe pkgs.starship} init nu > $out
+        '';
+      };
+
+      programs.starship = {
+        enable = true;
+        enableNushellIntegration = false;
+        settings = {
+          command_timeout = 100;
+          scan_timeout = 20;
+          character.error_symbol = "[➜](bold red)";
+          character.success_symbol = "[➜](bold green)";
+        };
+      };
+
+      programs.nushell = {
+        enable = true;
+        configFile.text = lib.readFile ./config.nu;
+        envFile.text = lib.readFile ./env.nu;
+      };
+    }
+  ];
+}
---
+++ modules/core/nushell/env.nu
@@ -0,0 +1,3 @@
+$env.LS_COLORS = (open ~/.config/nushell/ls_colors.txt)
+source ~/.config/nushell/zoxide.nu
+source ~/.config/nushell/starship.nu
